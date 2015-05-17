855cbbcc3bfe5797829e014f22cfd0c176f792a5
Siddharth Mishra <siddharth82mishra@gmail.com>
2015-05-17 00:57:14 -0600

    Adding Parse objects.

---
+++ WPF/SimpleChatApplication/Parse.Service/IParseObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse.Service
+{
+    public interface IChatParseObject
+    {
+        string ObjectId { get; set; }
+
+        Task<string> SaveObject();
+    }
+}
---
+++ WPF/SimpleChatApplication/Parse.Service/UserProfile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse.Service
+{
+    public class UserProfile : IChatParseObject
+    {
+        public string UserName { get; set; }
+        public string LoginId { get; set; }
+        public string Password { get; set; }
+        public string ObjectId { get; set; }
+        public string CellPhoneNumber { get; set; }
+
+        public UserProfile(string userName, string loginId, string password, string cellPhoneNumber)
+        {
+            this.UserName = userName;
+            this.LoginId = loginId;
+            this.Password = password;
+            this.ObjectId = Guid.NewGuid().ToString();
+        }
+
+        public async Task<string> SaveObject()
+        {
+            try
+            {
+                ParseObject newUser = new ParseObject("UserProfile");
+                newUser["ObjectId"] = this.ObjectId;
+                newUser["UserName"] = this.UserName;
+                newUser["LoginId"] = this.LoginId;
+                newUser["Password"] = this.Password;
+                newUser["CellPhoneNumber"] = this.CellPhoneNumber;
+                await newUser.SaveAsync();
+            }
+            catch (Exception exception)
+            {
+                
+                throw;
+            }
+            return ObjectId;
+        }
+    }
+}
